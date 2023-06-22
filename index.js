5df59a933924841149c5f1812c5e737cd0c545e5
peterkomo <mwangipeterkomo@gmail.com>
2023-06-22 19:05:57 +0300

    complited assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName ="bob"
+function getCustomerName() {
+    return customerName;
+  }
+function upperCaseCustomerName(bob) {
+     {
+      customerName = customerName.toUpperCase("bob");
+      
+    }
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  
+  function overwriteBestCustomer() {
+     bestCustomer= 'maybe bob';
+
+  }
+  const leastFavoriteCustomer = 'none';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'none'; }
